bda3d7149f4075a8657a995104103150033d70eb
Grace <gracenyangayi@gmail.com>
2022-06-21 21:46:00 +0300

    update index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // declaring elements
+    const searchBtn = document.getElementById('search-btn')
+    const mealList = document.getElementById('meal')
+
+    // adding event listerners
+    searchBtn.addEventListener('click', getMealList)
+    // get matching recipes 
+    function getMealList() {
+        let searchInputTxt = document.getElementById('search-input').value.trim()
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+            .then(res => res.json())
+            .then(data => {
+                let html = ""
+                if (data.meals) {
+                    data.meals.forEach(meal => {
+                        html += `
+                <div class = "meal-item" data-id = "${meal.idMeal}">
+                <div class ="meal-img">
+                <img src =${meal.strMealThumb} alt ="food">
+                </div>
+                <div class ="meal-name">
+                <h3>${meal.strMeal}</h3>
+                <a style="float:left; href = "#" class ="recipe-btn">Get Recipe</a>
+                <a style="float: right;" href="#" class="recipe-btn">Add Review</a>
+                </div>
+                </div>
+                `
+                    })
+
+                }
+                mealList.innerHTML = html
+            })
+
+    }
+
+
+
+})
